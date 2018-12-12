4f853f901dbc350c189c37056e5f9932c1c498c9
Hwanggyu Lim <hglim83@gmail.com>
2018-12-11 21:27:44 -0500

    Modify code: - In Study 2, two item bank size were created

--- Code/1.Generate Item Pool/Generate_item_pool.R
+++ Code/1.Generate Item Pool/Generate_item_pool.R
@@ -45,11 +45,12 @@ write.csv(stat_df1, "Input/stat_pool_1.csv")
 
 
 ## Study 2 --------------------------------------------
+# (1) item pool with 250 items 
 # set seed
 set.seed(377)
 
 # set the number of items
-nitem <- 400
+nitem <- 200
 
 # read MAPT Math data set
 mapt_dat <- read.csv("Input/Math2018_workbook.csv")
@@ -61,7 +62,7 @@ dat <- mapt_dat %>%
   select(a_FY18, b_FY18 , c_FY18)
 
 # randomly select 400 items for the item pool
-idx <- sort(sample(1:nrow(dat), 400, FALSE))
+idx <- sort(sample(1:nrow(dat), nitem, FALSE))
 pool_2 <- dat[idx, ]
 
 # generate content categories
@@ -82,6 +83,53 @@ Max <- round(apply(pool_2, 2, max), 2)
 stat_df2 <- data.frame(mu=mu, sigma=sigma, Min=Min, Max=Max)
 
 # export data
-saveRDS(pool_2, "Input/item_pool_2.rds")
-write.csv(stat_df2, "Input/stat_pool_2.csv")
+saveRDS(pool_2, "Input/item_pool_2_B200.rds")
+write.csv(stat_df2, "Input/stat_pool_2_B200.csv")
+
+## Study 2 --------------------------------------------
+# (1) item pool with 400 items 
+# set seed
+set.seed(771)
+
+# set the number of items
+nitem <- 400
+
+# read MAPT Math data set
+mapt_dat <- read.csv("Input/Math2018_workbook.csv")
+
+# filter out items with sample size greater than 500
+dat <- mapt_dat %>% 
+  filter(!is.na(a_FY18)) %>% 
+  filter(N.count.in.2018 >= 500) %>% 
+  select(a_FY18, b_FY18 , c_FY18)
+
+# randomly select 400 items for the item pool
+idx <- sort(sample(1:nrow(dat), nitem, FALSE))
+pool_2 <- dat[idx, ]
+
+# generate content categories
+cats1 <- c(rep(1, nitem * .25), rep(2, nitem * .25), rep(3, nitem * .25), rep(4, nitem * .25))
+class1 <- sample(x=cats1, size=nitem)
+cats2 <- c(rep(1, nitem * .25), rep(2, nitem * .25), rep(3, nitem * .50))
+class2 <- sample(x=cats2, size=nitem)
+
+# create a data.frame
+pool_2 <- data.frame(pool_2, class1=class1, class2=class2)
+names(pool_2) <- c("a", "b", "c", "class1", "class2")
+
+# descriptive statistics of item pool
+mu <- round(apply(pool_2, 2, mean), 2)
+sigma <- round(apply(pool_2, 2, sd), 2)
+Min <- round(apply(pool_2, 2, min), 2)
+Max <- round(apply(pool_2, 2, max), 2)
+stat_df2 <- data.frame(mu=mu, sigma=sigma, Min=Min, Max=Max)
+
+# export data
+saveRDS(pool_2, "Input/item_pool_2_B400.rds")
+write.csv(stat_df2, "Input/stat_pool_2_B400.csv")
+
+
+
+
+
 
